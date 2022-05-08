03f9fe47d0d8f82a12eebd222f4155522d91f74f
Pukkah <miksramans@gmail.com>
2022-05-09 01:44:05 +0300

    Add @Transactional and improve DataSourceConfiguration

--- src/main/java/io/codelex/flightplanner/model/Flight.java
+++ src/main/java/io/codelex/flightplanner/model/Flight.java
@@ -6,25 +6,34 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "flights", uniqueConstraints = { @UniqueConstraint(name = "flights_should_be_unique",
         columnNames = { "airport_from", "airport_to", "carrier", "departureTime", "arrivalTime" }) })
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Builder(toBuilder = true)
 public class Flight {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(foreignKey = @ForeignKey(name = "airport_from_fk_airport"),
             name = "airport_from", referencedColumnName = "airport")
     private Airport from;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(foreignKey = @ForeignKey(name = "airport_to_fk_airport"),
             name = "airport_to", referencedColumnName = "airport")
     private Airport to;
--- src/main/java/io/codelex/flightplanner/service/AirportService.java
+++ src/main/java/io/codelex/flightplanner/service/AirportService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,11 +16,13 @@ import java.util.List;
 public class AirportService {
     private final AirportRepository airportRepository;
 
+    @Transactional
     public Airport getOrCreate(Airport airport) {
         return airportRepository.findById(airport.getAirport())
                                 .orElseGet(() -> airportRepository.save(airport));
     }
 
+    @Transactional(readOnly = true)
     public List<Airport> searchAirports(String search) {
         search = search.trim();
         Airport airport = Airport.builder().airport(search).city(search).country(search).build();
@@ -29,6 +32,7 @@ public class AirportService {
         return airportRepository.findAll(Example.of(airport, airportMatcher));
     }
 
+    @Transactional
     public void deleteAllAirports() {
         airportRepository.deleteAll();
     }
